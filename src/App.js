e3dd929bc6f33a3448531c0136179c40bfed88f3
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-03-01 23:07:43 -0600

    rendered some monsters names

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
-function App() {
-  return <div className="app">Monsters</div>;
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: "Frankestein",
+          id: "asdasddv",
+        },
+        {
+          name: "Dracula",
+          id: "sgsdfg",
+        },
+        {
+          name: "Zombie",
+          id: "srbdfvg",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
