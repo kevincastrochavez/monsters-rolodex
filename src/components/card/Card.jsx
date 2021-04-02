f473912db0ee39686855be07a31b90cadcbf66f3
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-02 12:03:41 -0600

    setup enzyme and created the first shallow test

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+
 import "./card.css";
 
-function Card({ monster }) {
+function Card({ monster: { id, name, email } }) {
   return (
     <div className="card-container">
       <img
-        src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
         alt="monster"
       />
 
-      <h2>{monster.name}</h2>
+      <h2>{name}</h2>
 
-      <p>{monster.email}</p>
+      <p>{email}</p>
     </div>
   );
 }
